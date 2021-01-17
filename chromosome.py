fix crossover operator, fitness calculation, chromosome and population initialization

chromosome.py
@@ -7,7 +7,7 @@ class Chromosome:
     def __init__(self, chromosome_size, distances):
         self.chromosome_size = chromosome_size
         self.genes = np.empty(self.chromosome_size, dtype=int)
-        self.fitness = -math.inf
+        self.fitness = math.inf
 
         self.distances = distances
 
@@ -18,23 +18,21 @@ class Chromosome:
         costs_sum = 0.0
 
         points_size = int(np.ceil(self.distances[0].size / self.genes.size))
-
-        tmp = np.array([[-1 for _ in range(points_size)] for _ in range(self.genes.size)])
+        deprecated = np.copy(self.genes)
 
         for i, gene in enumerate(self.genes):
             for j in range(points_size):
                 if j == 0:
-                    tmp[i][0] = gene
                     costs_sum += self.distances[gene][0]
                     continue
 
                 cur_min_ind = -1
                 cur_min = math.inf
                 for k, el in enumerate(self.distances[gene]):
-                    if gene != k and el < cur_min and k not in tmp:
+                    if gene != k and el < cur_min and k not in deprecated:
                         cur_min = el
                         cur_min_ind = k
-                tmp[i][j] = cur_min_ind
+                deprecated = np.append(deprecated, cur_min_ind)
                 costs_sum += self.distances[gene][cur_min_ind]
 
         self.fitness = costs_sum

main.py
@@ -18,8 +18,8 @@ alpha2 = 0.5
 # Acceptable TSPTW subproblem size Z
 Z = 4
 
-# Population size, i. g. number of chromosomes in population
-P = 15
+# Population size, i. e. number of chromosomes in population
+P = 5
 
 # Number of all generations
 ng = 15
@@ -35,6 +35,7 @@ dpi_standart = 400
 linewidth_standart = 0.5
 
 width = depth = 0.5
+
 
 # fix wrong z-offsets in 3d plot
 def _get_coord_info_new(self, renderer):
@@ -86,10 +87,6 @@ def solve_test():
     test_dataset_points = test_dataset[1:][:]
     tws_reduced = tws_all[1:]
 
-    # print(spatiotemporal_points_dist)
-    # print(test_dataset_points)
-    # plot_with_tws(test_dataset, spatiotemporal.tws_all, spatiotemporal.MAX_TW)
-
     solver = Solver(Z, spatiotemporal_points_dist, P, ng, Pc, Pm)
     result = solver.solve()
 
@@ -97,6 +94,9 @@ def solve_test():
     res_tws = np.array([[tws_reduced[point] for point in cluster] for cluster in result])
 
     plot_clusters(test_dataset_points, res_dataset, res_tws, spatiotemporal.MAX_TW)
+
+    print(res_dataset)
+    print(res_tws)
 
 
 def plot_clusters(init_dataset, dataset, tws, max_tw):

population.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 import numpy as np
@@ -13,14 +14,17 @@ class Population:
 
         self.best_chromosome_ind = -1
 
-        self.chromosomes = np.empty(self.population_size, dtype=Chromosome)
-        self.chromosomes.fill(Chromosome(chromosome_size, self.distances))
+        self.chromosomes = np.array([])
+        for _ in range(self.population_size):
+            self.chromosomes = np.append(self.chromosomes, Chromosome(chromosome_size, self.distances))
 
     def generate_random_population(self):
+        # Generate random genes for every chromosome
         for chromosome in self.chromosomes:
             chromosome.generate_random_chromosome()
 
     def calculate_fitness(self):
+        # Calculate fitness for every chromosome in population
         for chromosome in self.chromosomes:
             chromosome.calculate_fitness()
 
@@ -51,31 +55,39 @@ class Population:
         return new_population
 
     def crossover(self, prob):
-        if prob > random.random():
-            rand_parents_ind = np.random.choice(np.arange(0, self.population_size), replace=False, size=2)
+        for i in range(int(np.ceil(self.population_size / 2))):
+            if prob > random.random():
+                rand_parents_ind = np.random.choice(np.arange(0, self.population_size), replace=False, size=self.population_size)
 
-            cut_point_ind = random.randint(1, self.chromosomes[0].chromosome_size - 1)
+                cut_point_ind = random.randint(1, self.chromosomes[0].chromosome_size - 1)
 
-            parent1 = self.chromosomes[rand_parents_ind[0]]
-            parent2 = self.chromosomes[rand_parents_ind[1]]
+                parent1 = self.chromosomes[rand_parents_ind[i]]
+                parent2 = self.chromosomes[rand_parents_ind[self.population_size - i - 1]]
 
-            child1 = Chromosome(self.chromosomes[0].chromosome_size, self.distances)
-            child2 = Chromosome(self.chromosomes[0].chromosome_size, self.distances)
+                child1 = Chromosome(self.chromosomes[0].chromosome_size, self.distances)
+                child2 = Chromosome(self.chromosomes[0].chromosome_size, self.distances)
 
-            for _ in range(self.chromosomes[0].chromosome_size):
-                child1.genes[:cut_point_ind] = parent1.genes[:cut_point_ind]
-                child1.genes[cut_point_ind:] = parent2.genes[cut_point_ind:]
+                for _ in range(self.chromosomes[0].chromosome_size):
+                    child1.genes[:cut_point_ind] = parent1.genes[:cut_point_ind]
+                    child1.genes[cut_point_ind:] = parent2.genes[cut_point_ind:]
 
-                child2.genes[:cut_point_ind] = parent2.genes[:cut_point_ind]
-                child2.genes[cut_point_ind:] = parent1.genes[cut_point_ind:]
+                    child2.genes[:cut_point_ind] = parent2.genes[:cut_point_ind]
+                    child2.genes[cut_point_ind:] = parent1.genes[cut_point_ind:]
 
-            self.chromosomes[rand_parents_ind[0]] = child1
-            self.chromosomes[rand_parents_ind[1]] = child2
+                self.chromosomes[rand_parents_ind[i]] = child1
+                self.chromosomes[rand_parents_ind[self.population_size - i - 1]] = child2
 
     def mutate(self, prob):
         for chromosome in self.chromosomes:
             if prob > random.random():
                 chromosome.mutate()
 
-    def find_best_chrom(self):
-        self.best_chromosome_ind = np.argmax(np.array([el.fitness for el in self.chromosomes]))
+    def find_best_chromosome(self):
+        min_fitness = math.inf
+        min_ind = -1
+        for i, chromosome in enumerate(self.chromosomes):
+            if chromosome.fitness < min_fitness:
+                min_fitness = chromosome.fitness
+                min_ind = i
+
+        return self.chromosomes[min_ind]

solver.py
@@ -2,6 +2,7 @@ import math
 
 import numpy as np
 
+from chromosome import Chromosome
 from population import Population
 
 
@@ -21,15 +22,18 @@ class Solver:
         self.Pc = Pc
         self.Pm = Pm
 
-        self.populations = np.empty(self.ng, dtype=Population)
-        self.populations.fill(Population(self.P, self.k, self.distances))
+        self.populations = np.array([])
+        for _ in range(self.ng):
+            self.populations = np.append(self.populations, Population(self.P, self.k, self.distances))
+
+        self.best_chromosome = Chromosome(self.k, self.distances)
 
     def make_cluster_from_medoids(self):
-        medoids = self.populations[self.ng - 1].chromosomes[self.populations[self.ng - 1].best_chromosome_ind].genes
-
+        medoids = self.best_chromosome.genes
         points_size = int(np.ceil(self.distances[0].size / medoids.size))
-
         result = np.array([[-1 for _ in range(points_size)] for _ in range(medoids.size)])
+
+        deprecated = np.copy(medoids)
 
         for i, gene in enumerate(medoids):
             for j in range(points_size):
@@ -40,30 +44,44 @@ class Solver:
                 cur_min_ind = -1
                 cur_min = math.inf
                 for k, el in enumerate(self.distances[gene]):
-                    if gene != k and el < cur_min and k not in result:
+                    if gene != k and el < cur_min and k not in deprecated:
                         cur_min = el
                         cur_min_ind = k
                 result[i][j] = cur_min_ind
+                deprecated = np.append(deprecated, cur_min_ind)
 
         return result
 
     def solve(self):
-
         self.populations[0].generate_random_population()
         self.populations[0].calculate_fitness()
+        self.save_new_best_chromosome(self.populations[0])
 
-        print(self.populations[0].chromosomes[0].fitness)
-        print(self.populations[0].chromosomes[1].fitness)
+        print("---------")
+        print("Iteration {}".format(0))
+        print("Best chromosome fitness {}".format(self.best_chromosome.fitness))
+        print("Best genes {}".format(self.best_chromosome.genes))
+        print("---------")
 
         for i in range(1, self.ng):
-
             self.populations[i] = self.populations[i - 1].selection()
             self.populations[i].crossover(self.Pc)
             self.populations[i].mutate(self.Pm)
 
-            self.populations[i].calculate_fitness()
+            print([chromosome.genes for chromosome in self.populations[i].chromosomes])
 
-            print(self.populations[1].chromosomes[0].fitness)
-            print(self.populations[1].chromosomes[1].fitness)
+            self.populations[i].calculate_fitness()
+            self.save_new_best_chromosome(self.populations[i])
+
+            print("---------")
+            print("Iteration {}".format(i))
+            print("Best chromosome fitness {}".format(self.best_chromosome.fitness))
+            print("Best genes {}".format(self.best_chromosome.genes))
+            print("---------")
 
         return self.make_cluster_from_medoids()
+
+    def save_new_best_chromosome(self, population):
+        chrom_fitness = population.find_best_chromosome().fitness
+        if chrom_fitness < self.best_chromosome.fitness:
+            self.best_chromosome = population.find_best_chromosome()